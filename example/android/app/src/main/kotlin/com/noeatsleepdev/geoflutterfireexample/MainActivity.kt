a7b377e7909892a30d7e9ab34569c7998a20a39c
Hemedy <32874926+Hemedy@users.noreply.github.com>
2024-11-25 17:24:19 +0300

    cloud_firestore: ^3.1.6

--- example/android/app/src/main/kotlin/com/noeatsleepdev/geoflutterfireexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/noeatsleepdev/geoflutterfireexample/MainActivity.kt
@@ -2,7 +2,8 @@ package com.noeatsleepdev.geoflutterfireexample
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
+//import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
